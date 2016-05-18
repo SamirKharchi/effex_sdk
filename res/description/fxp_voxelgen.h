cafac62ae432983a20c0fbdab703e4f77bb18dbc
Navié OS <info@dpit2.de>
2016-05-18 19:41:13 +0200

    Add files via upload
    resources for voxel generator & channel sampling effector

---
+++ res/description/fxp_voxelgen.h
@@ -0,0 +1,14 @@
+#ifndef _fxp_voxelgen_H_
+#define _fxp_voxelgen_H_
+
+enum
+{
+	//Use ids between 1000 - 1099
+	VGEN_CHANNEL = 1000,
+	VGEN_CHANNEL_SELECTOR,
+	VGEN_SCALE,
+	VGEN_SCENE,
+	VGEN_THRESHOLD
+};
+
+#endif
